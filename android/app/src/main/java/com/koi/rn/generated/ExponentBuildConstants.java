2c10dad0ef04e4fbd4f631cd23ef3d5196fc723f
linhnguyen <linh.nguyen@hoanghacgroup.com>
2017-05-26 01:00:57 +0700

    Expokit

---
+++ android/app/src/main/java/com/koi/rn/generated/ExponentBuildConstants.java
@@ -0,0 +1,8 @@
+package com.koi.rn.generated;
+
+// This file is auto-generated. Please don't rename!
+public class ExponentBuildConstants {
+
+  public static final String DEVELOPMENT_URL = "http://192.168.1.7:19001";
+
+}
